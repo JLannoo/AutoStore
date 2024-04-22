074cae0ededf20b42f68b9096fda213be1395077
JLannoo <julianlannoo@gmail.com>
2024-04-22 18:17:10 -0300

    Add project files.

---
+++ AutoStore/ModEntry.cs
@@ -0,0 +1,13 @@
+﻿using StardewModdingAPI;
+
+namespace AutoStore;
+
+internal class ModEntry : Mod {
+    public static IMonitor monitor;
+
+    public override void Entry(IModHelper helper){
+        monitor = Monitor;
+    }
+
+
+}
